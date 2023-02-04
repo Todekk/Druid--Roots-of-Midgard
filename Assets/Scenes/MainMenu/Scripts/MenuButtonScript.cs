19639186047b83c970259ed8bf41cc1ba63c1d17
VeselinDenchev <den4ev01@gmail.com>
2023-02-04 22:52:59 +0200

    Add Main menu buttons hover sound

--- Assets/Scenes/MainMenu/Scripts/MenuButtonScript.cs
+++ Assets/Scenes/MainMenu/Scripts/MenuButtonScript.cs
@@ -6,14 +6,22 @@ using UnityEngine.EventSystems;
 public class MenuButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public TextMeshProUGUI text;
+    public AudioSource clickSound;
+    public AudioSource hoverSound;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (!clickSound.isPlaying)
+        {
+            hoverSound.Play();
+        }
+
         text.color = Color.black;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        hoverSound.Stop();
         text.color = Color.white;
     }
 }
